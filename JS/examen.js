9f136913fbe15ad572d189ec8cebc381cbca80e0
Jessica Arias <jessie.arias7@gmail.com>
2018-05-06 19:21:15 -0500

    Examen Jessica Arias

---
+++ JS/examen.js
@@ -0,0 +1,55 @@
+function myFunction1() {
+    document.getElementById("myDIV1").classList.toggle("ocultar");
+}
+
+function myFunction2() {
+    document.getElementById("myDIV2").classList.toggle("ocultar");
+}
+
+function myFunction3() {
+    document.getElementById("myDIV3").classList.toggle("ocultar");
+}
+
+function myFunction4() {
+    document.getElementById("myDIV4").classList.toggle("ocultar");
+}
+
+function myFunction5() {
+    document.getElementById("myDIV5").classList.toggle("ocultar");
+}
+
+function myFunction6() {
+    document.getElementById("myDIV6").classList.toggle("ocultar");
+}
+
+
+function myFunction7() {
+    document.getElementById("myDIV7").classList.toggle("ocultar");
+}
+
+
+function myFunction8() {
+    document.getElementById("myDIV8").classList.toggle("ocultar");
+}
+
+
+function myFunction9() {
+    document.getElementById("myDIV9").classList.toggle("ocultar");
+}
+
+
+function myFunction10() {
+    document.getElementById("myDIV10").classList.toggle("ocultar");
+}
+
+
+function myFunction11() {
+    document.getElementById("myDIV11").classList.toggle("ocultar");
+}
+
+function myFunction12() {
+    document.getElementById("myDIV12").classList.toggle("ocultar");
+}
+
+
+
